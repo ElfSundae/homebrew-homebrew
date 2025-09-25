f4d4e570694bd7285c33c9b5cc5c1246d5657bbf
Elf Sundae <elf.sundae@gmail.com>
2025-09-26 03:20:47 +0800

    Update backup-keyboard-shortcuts to 0.1.1

--- Formula/backup-keyboard-shortcuts.rb
+++ Formula/backup-keyboard-shortcuts.rb
@@ -1,8 +1,8 @@
 class BackupKeyboardShortcuts < Formula
   desc "Backup and restore user-defined keyboard shortcuts"
   homepage "https://github.com/ElfSundae/backup-keyboard-shortcuts"
-  url "https://github.com/ElfSundae/backup-keyboard-shortcuts/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "1da584ea699334c71e1e92360acb3f8eca8f9a91c8ae16f38b42f52e9d7fd16d"
+  url "https://github.com/ElfSundae/backup-keyboard-shortcuts/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "90faccf64c0fe8099c3e7b1e98e374d86de095324da92432bad5fb7812e6b55c"
   license "MIT"
 
   depends_on "jq"
